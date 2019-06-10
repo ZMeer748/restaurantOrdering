66139f5c396dbcde429873e8744af54858d0131f
SissonMore <sissonmore@163.com>
2019-06-10 12:46:10 +0800

    menu content test

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,13 +1,51 @@
 package menu;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import org.junit.Test;
 
 public class Menu {
 
-	ArrayList<MenuItem> itemList = new ArrayList<>();
+	static ArrayList<MenuItem> itemList = new ArrayList<>();
 
-	void getList_test() {
-		
+	public static void main(String[] args) {
+		getList_test();
+	}
+
+	@Test
+	static void getList_test() {
+		getMenuContent("menu.txt");
+		for (MenuItem item : itemList) {
+			System.out.println(item.code);
+		}
+	}
+
+	static private void getMenuContent(String filename) {
+		try {
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
+			String str = null;
+			int tempCode;
+			while ((str = br.readLine()) != null) {
+				MenuItem tempItem;
+				StringTokenizer tokens = new StringTokenizer(str, "/");
+				tempCode = Integer.parseInt(tokens.nextToken());
+				tempItem = new MenuItem(tempCode, tokens.nextToken(), Double.parseDouble(tokens.nextToken()));
+				itemList.add(tempItem);
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			// e.printStackTrace();
+			System.out.println("We lose the data for the menu.");
+		} catch (NumberFormatException | IOException e) {
+			// e.printStackTrace();
+			System.out.println("There is something wrong.");
+		}
 	}
 
 }
--- src/menu/MenuItem.java
+++ src/menu/MenuItem.java
@@ -2,7 +2,18 @@ package menu;
 
 public class MenuItem {
 
-	String code, name, sort;
+	public MenuItem(int code, String name, double price) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		if (code > 20)
+			sort = "Food";
+		else
+			sort = "Drink";
+	}
+
+	int code;
+	String name, sort;
 	double price;
 
 }
