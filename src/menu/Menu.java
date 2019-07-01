6846f81ee266719dd2d3afdc1f98012fbb98086e
SissonMore <sissonmore@163.com>
2019-07-01 19:53:54 +0800

    a0.3

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,12 +1,12 @@
 package menu;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+// import java.io.BufferedReader;
+// import java.io.FileNotFoundException;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.net.URL;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+// import java.util.StringTokenizer;
 
 import db.DBInteraction_Menu;
 
@@ -26,31 +26,19 @@ public class Menu {
 
 	// static private void getMenuContent(String filename) {
 	/*
-	static private void getMenuContent(URL url) {
-		System.out.println(url);
-		try {
-			// FileReader fr = new FileReader(url.getPath());
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-			// File file = new File(url);
-			String str = null;
-			int tempCode;
-			while ((str = br.readLine()) != null) {
-				MenuItem tempItem;
-				StringTokenizer tokens = new StringTokenizer(str, "/");
-				tempCode = Integer.parseInt(tokens.nextToken());
-				tempItem = new MenuItem(tempCode, tokens.nextToken(), Double.parseDouble(tokens.nextToken()));
-				itemList.add(tempItem);
-			}
-			br.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("We lose the data for the menu.");
-			e.printStackTrace();
-		} catch (NumberFormatException | IOException e) {
-			System.out.println("There is something wrong.");
-			e.printStackTrace();
-		}
-	}
-	*/
+	 * static private void getMenuContent(URL url) { System.out.println(url); try {
+	 * // FileReader fr = new FileReader(url.getPath()); BufferedReader br = new
+	 * BufferedReader(new InputStreamReader(url.openStream())); // File file = new
+	 * File(url); String str = null; int tempCode; while ((str = br.readLine()) !=
+	 * null) { MenuItem tempItem; StringTokenizer tokens = new StringTokenizer(str,
+	 * "/"); tempCode = Integer.parseInt(tokens.nextToken()); tempItem = new
+	 * MenuItem(tempCode, tokens.nextToken(),
+	 * Double.parseDouble(tokens.nextToken())); itemList.add(tempItem); }
+	 * br.close(); } catch (FileNotFoundException e) {
+	 * System.out.println("We lose the data for the menu."); e.printStackTrace(); }
+	 * catch (NumberFormatException | IOException e) {
+	 * System.out.println("There is something wrong."); e.printStackTrace(); } }
+	 */
 
 	public static int getNum() {
 		return itemList.size();
--- src/property/PropertiesProcess.java
+++ src/property/PropertiesProcess.java
@@ -1,6 +1,5 @@
 package property;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
